9c47ff11a7d5fa822edd7854a6ce410d9ce70e4c
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2024-06-13 14:58:35 +0200

    remove unnecesarry page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import { Button } from '@mui/material';
 export default function Home() {
   return (
     <main>
-      <Button href='/test' variant='outlined'>
-        Go to testpage
-      </Button>
       <Button variant='contained'>Hello World!</Button>
     </main>
   );
